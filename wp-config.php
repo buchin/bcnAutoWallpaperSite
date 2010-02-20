aec6405b1716d935d2ed16ef66463688151d1de5
Mochammad Masbuchin <buchin@masbuchin.com>
2010-02-19 20:16:44 -0700

    Removing accidentally put credential

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'iphonewallpapers');
+define('DB_NAME', '');
 
 /** MySQL database username */
-define('DB_USER', 'iphonewallpapers');
+define('DB_USER', '');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'superbaka');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
-define('DB_HOST', 'buchin.db');
+define('DB_HOST', '');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
